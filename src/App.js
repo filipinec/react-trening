77ecc28d573e1cf54f7ade5b727b36ed5bb9cd6f
Filip Tasevski <135996963+filipinec@users.noreply.github.com>
2024-02-26 21:00:44 +0100

    Using Previous State - Delete events

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-const [name, setName] = useState('Filip')
+
 const [events, setEvents] = useState([
   {title: "Filip have birthday", id:1},
   {title: "JavaScript is programming language", id:2},
   {title: "Liverpool win trophy", id:3}
   
 ])
-const handleClick = () => {
-  setName('Gabriela')
+const handleClick = (id) => {
+setEvents((prevEvents) => {
+  return prevEvents.filter((event) => {
+    return id !== event.id
+  })
+})
+
   
 }
   return (
     <div className="App">
-     <h1>My name is {name}</h1>
-     <button onClick={handleClick}>Change Name</button>
      {events.map((event, index) => (
       // use key for have unique id
       <div key={event.id}> 
         <h2>{index+1} - {event.title}</h2>
+        <button onClick={() => handleClick(event.id)}>Delete</button>
       </div>
      ))}
     </div>
