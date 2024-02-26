44bdc4f21c0588ef8f08124f5b92a3cef27b7326
Filip Tasevski <135996963+filipinec@users.noreply.github.com>
2024-02-26 20:31:33 +0100

    Outputtung Lists

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import { useState } from 'react';
 
 function App() {
 const [name, setName] = useState('Filip')
+const [events, setEvents] = useState([
+  {title: "Filip have birthday", id:1},
+  {title: "JavaScript is programming language", id:2},
+  {title: "Liverpool win trophy", id:3}
+  
+])
 const handleClick = () => {
   setName('Gabriela')
   
@@ -11,7 +17,14 @@ const handleClick = () => {
     <div className="App">
      <h1>My name is {name}</h1>
      <button onClick={handleClick}>Change Name</button>
+     {events.map((event, index) => (
+      // use key for have unique id
+      <div key={event.id}> 
+        <h2>{index+1} - {event.title}</h2>
+      </div>
+     ))}
     </div>
+
   );
   
 }
