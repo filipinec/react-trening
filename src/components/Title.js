8b11dfe7049f93225b31b5f514f4382696ef27a6
Filip Tasevski <135996963+filipinec@users.noreply.github.com>
2024-02-27 22:04:38 +0100

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,10 @@
 const Title = (props) => {
   return (
-    <div>
+    <>
       <h1 className='title'>{props.title}</h1>
       <br />
       <h2 className='subtitle'>{props.subtitle}</h2>
-    </div>
+    </>
   )
 }
 
