81ad8b52e56d7798c2f034cbd15308302724a6de
Filip Tasevski <135996963+filipinec@users.noreply.github.com>
2024-03-02 12:47:10 +0100

    onChange Events

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import  './NewEventForm.css'
 
 export const NewEventForm = () => {
+  const[title, setTitle] =useState('')
+  const[date, setDate] = useState('')
+  
   return (
     <form className='new-event-form'>
       <label>
         <span>Event Title:</span>
-        <input type="text"/>
+        <input type="text" onChange={(e) => setTitle(e.target.value)}/>
       </label>
       <label>
         <span>Event Date:</span>
-        <input type="date"/>
+        <input type="date" onChange={(e) => setDate(e.target.value)}/>
       </label>
       <button>Submit</button>
+      <p>title - {title}
+      <br />date - {date}</p>
     </form>
   )
 }
